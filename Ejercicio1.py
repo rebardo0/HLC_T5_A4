315e38e47f2160dafecb0bd787ee0677a298b7a0
rebardo0 <svelabeltran@gmail.com>
2025-02-05 12:37:24 +0100

    En proceso

---
+++ Ejercicio1.py
@@ -0,0 +1,12 @@
+
+def factorial_numero(numero):
+    factorial=1
+
+    for n in range(1,numero+1):
+        factorial=factorial*n
+
+    return factorial
+
+numero=int(input("Introduce un número: "))
+resultado=factorial_numero(numero)
+print(resultado)
