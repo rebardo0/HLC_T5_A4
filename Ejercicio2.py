506921d3c0cd08fc72d3bc5edf5044f13ce783f2
rebardo0 <svelabeltran@gmail.com>
2025-02-05 13:08:09 +0100

    En proceso

---
+++ Ejercicio2.py
@@ -0,0 +1,8 @@
+def Fibonacci(numero):
+    
+    
+    return 
+
+numero=int(input("Ingrese un numero: "))
+resultado=Fibonacci(numero)
+print(resultado)
---
+++ Ejercicio3.py
@@ -0,0 +1,11 @@
+def potencia_numero(a,b):
+    potencia=a
+    for i in range(1,b):
+        potencia=potencia*a
+
+    return potencia
+
+numero=int(input("Ingrese un numero: "))
+numero2=int(input("Ingrese la potencia del numero: "))
+resultado=potencia_numero(numero,numero2)
+print(resultado)
