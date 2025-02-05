1f30da13c56e0c32a98fd3e8823c12e8bcf63a45
rebardo0 <svelabeltran@gmail.com>
2025-02-05 13:24:17 +0100

    En proceso

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,7 +1,10 @@
 def Fibonacci(numero):
-    
-    
-    return 
+    if numero==0:
+        return 0
+    if numero==1:
+        return 1
+    else:
+        return Fibonacci(numero-1) + Fibonacci(numero-2)
 
 numero=int(input("Ingrese un numero: "))
 resultado=Fibonacci(numero)
